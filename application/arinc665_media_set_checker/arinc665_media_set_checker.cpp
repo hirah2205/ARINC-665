2d34aebbba3858c621ffdabf3ac94fb935122096
Thomas Vogt <thomas@thomas-vogt.de>
2017-05-02 11:24:26 +0000

    * added ARINC 665 Media Set checker app basis

---
+++ application/arinc665_media_set_checker/arinc665_media_set_checker.cpp
@@ -0,0 +1,30 @@
+/*
+ * $Date$
+ * $Revision$
+ */
+/**
+ * @file
+ * @copyright
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * @author @todo Add author
+ *
+ * @brief @todo Add brief description
+ **/
+
+/**
+ * tasks:
+ * # check integrity of all files
+ * # check integrity of all loads
+ * # check integrity of all batches
+ *
+ **/
+
+int main()
+{
+  return -1;
+}
+
+
