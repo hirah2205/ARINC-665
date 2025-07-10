cb8b9fba8eab39c72760a200f469fd5c01a17dc1
Thomas Vogt <thomas@thomas-vogt.de>
2025-07-11 00:32:20 +0200

    feature: Add dependency graph generation

---
+++ CMakeGraphVizOptions.cmake
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: MPL-2.0
+
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set( GRAPHVIZ_EXECUTABLES FALSE )
+set( GRAPHVIZ_EXTERNAL_LIBS FALSE )
+set( GRAPHVIZ_CUSTOM_TARGETS FALSE )
+
+set( GRAPHVIZ_GENERATE_PER_TARGET FALSE )
+set( GRAPHVIZ_GENERATE_DEPENDERS FALSE )
+set( GRAPHVIZ_IGNORE_TARGETS ".*_test" )
